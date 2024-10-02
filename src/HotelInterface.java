518034b6da44bf7257ebe1c2d205a1fc25f35863
Guinn <eguinjonathan08@gmail.com>
2024-10-02 17:13:12 +0700

    Add checkOut method to HotelInterface

--- src/HotelInterface.java
+++ src/HotelInterface.java
@@ -4,5 +4,5 @@ public interface HotelInterface {
     public void orderRoom(String roomName, String customerName);
     public void checkAvailability(String roomName);
     public void orderDetails(String roomName);
-    
+    public void checkOut (String roomName, String customerName);
 }
